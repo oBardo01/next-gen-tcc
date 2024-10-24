bddb2b87eb081cae3d57c27a8af6b0b816cc8972
Pedro Abreu <96831678+oBardo01@users.noreply.github.com>
2024-10-24 20:09:25 -0300

    tentativa 39

--- app.js
+++ app.js
@@ -39,6 +39,16 @@ server.use('/src', express.static(path.join(__dirname, '/src')));
 server.use('/db', express.static(path.join(__dirname, '/db')));
 server.use(auth(config));
 server.set('views', path.join(__dirname, '/views'));
+app.use(
+    auth({
+      issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+      baseURL: process.env.BASE_URL,
+      clientID: process.env.AUTH0_CLIENT_ID,
+      secret: process.env.SESSION_SECRET,
+      authRequired: false,
+      auth0Logout: true,
+    }),
+  );
 
 
 server.get('/', async (req, res) => {
